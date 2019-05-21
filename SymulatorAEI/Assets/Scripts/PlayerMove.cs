bdbfdd7b40ad6780d70187a9f519ae84ed837bf6
Kamil Polok <kamil.polokpl@gmail.com>
2019-05-21 22:35:53 +0200

    Dodano skakanie, nie działa na schodach.

--- SymulatorAEI/Assets/Scripts/PlayerMove.cs
+++ SymulatorAEI/Assets/Scripts/PlayerMove.cs
@@ -7,12 +7,16 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private string horizontalInputName;
     [SerializeField] private string verticalInputName;
     [SerializeField] private float movementSpeed;
+    [SerializeField] private float velocityY;
 
     private CharacterController charController;
+    [SerializeField] private bool jumped;
  
     private void Awake()
     {
         charController = GetComponent<CharacterController>();
+        jumped = false;
+        velocityY = 0;
     }
 
     private void Update()
@@ -28,6 +32,34 @@ public class PlayerMove : MonoBehaviour
         Vector3 forwardMovement = transform.forward * vertInput;
         Vector3 rightMovement = transform.right * horizInput;
 
-        charController.SimpleMove(forwardMovement + rightMovement);
+        //Movement vector in total
+        Vector3 movement = forwardMovement + rightMovement;
+        movement.y += velocityY;    //Movement in Y axis
+        //if jumped add force to Y axis
+        if (Input.GetKeyDown("space") && jumped == false)
+        {
+            velocityY += 5;
+            Debug.Log("JUMP");
+            jumped = true;
+        }
+        //after jump add gravity to Y movement
+        if (jumped == true)
+        {
+            velocityY += Physics.gravity.y * Time.fixedDeltaTime;
+        }
+
+        
+        charController.Move(movement * Time.deltaTime);
+    }
+
+    //reset jump on collision with ground
+    //doesnt work on stairs
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.tag == "Ground")
+        {
+            jumped = false;
+            velocityY = 0;
+        }
     }
 }
