f6839236463d4ed4eb2c06840aca60c6cfcdc904
yamaz0 <szulc.patryk1c@gmail.com>
2019-05-21 18:14:45 +0200

    Shader Reflection

---
+++ SymulatorAEI/Assets/Scripts/Reflection.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Reflection : MonoBehaviour
+{
+    // Attach this script to an object that uses a Reflective shader.
+    // Realtime reflective cubemaps!
+
+    int cubemapSize = 128;
+    bool oneFacePerFrame = false;
+    Camera cam;
+    Material _MaterialShader;
+    RenderTexture renderTexture;
+
+    [Header("Wyłącz i włącz skrypt żeby nadpisać zmiany z suwaków/kolorów")]
+    [Range(0.01f,1)]
+    public float inten1;//intensywnosc 1
+    [Range(0.01f, 1)]
+    public float inten2;//intensywnosc 2
+
+    public Color col1;
+    public Color col2;
+
+    void Start()
+    {
+        // render all six faces at startup
+        UpdateCubemap(63);
+    }
+
+    void OnDisable()
+    {
+        DestroyImmediate(cam);
+        DestroyImmediate(renderTexture);
+        DestroyImmediate(_MaterialShader);
+    }
+
+    void LateUpdate()
+    {
+        if (oneFacePerFrame)
+        {
+            var faceToRender = Time.frameCount % 6;
+            var faceMask = 1 << faceToRender;
+            UpdateCubemap(faceMask);
+        }
+        else
+        {
+            UpdateCubemap(63); // all six faces
+        }
+    }
+
+    void UpdateCubemap(int faceMask)
+    {
+        if (!cam)
+        {
+            GameObject obj = new GameObject("CubemapCamera", typeof(Camera));
+            obj.hideFlags = HideFlags.HideAndDontSave;
+            obj.transform.position = transform.position;
+            obj.transform.rotation = Quaternion.identity;
+            cam = obj.GetComponent<Camera>();
+            cam.farClipPlane = 100; // don't render very far into cubemap
+            cam.enabled = false;
+        }
+
+        if (!renderTexture)
+        {
+            renderTexture = new RenderTexture(cubemapSize, cubemapSize, 16);
+            renderTexture.dimension = UnityEngine.Rendering.TextureDimension.Cube;
+            renderTexture.hideFlags = HideFlags.HideAndDontSave;
+            //GetComponent<Renderer>().sharedMaterial.SetTexture("_Cube", renderTexture);
+        }
+
+        cam.transform.position = transform.position;
+        cam.RenderToCubemap(renderTexture, faceMask);
+
+        if (_MaterialShader == null)
+        {
+            _MaterialShader = new Material(Shader.Find("Custom/Reflection")); //stworzenie materialu ktory bedzie obslugiwal shader
+            _MaterialShader.hideFlags = HideFlags.HideAndDontSave; //ukrycie materialu z shaderem
+            GetComponent<Renderer>().material = _MaterialShader; //dodanie shadera do obiektu
+            _MaterialShader.SetFloat("_Inten", inten1); //nadanie intensywnosci pierwszej 
+            _MaterialShader.SetFloat("_Inten2", inten2); //nadanie intensywnosci drugiej 
+            _MaterialShader.SetColor("_Color", col1);//nadanie  koloru pierwszego
+            _MaterialShader.SetColor("_Color2", col2);//nadanie  koloru drugiego
+        }
+        _MaterialShader.SetTexture("_Cube", renderTexture); //dodanie cubemapa do shadera
+
+    }
+}
---
+++ SymulatorAEI/Assets/Scripts/Reflection.shader
@@ -0,0 +1,98 @@
+﻿Shader "Custom/Reflection"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+		_Cube("Cube", Cube) = "" {}
+
+		_Inten("Inten1", Range(0.01, 1)) = 0.5
+		_Inten2("Inten2", Range(0.01, 1)) = 0.5
+		_Color("Main Color", Color) = (1,1,1)
+		_Color2("Secound Color", Color) = (0.5,0.5,1)
+	}
+		SubShader
+		{
+			Tags { "RenderType" = "Opaque" }
+			LOD 100
+
+			//ZWrite Off
+			//Blend SrcAlpha OneMinusSrcAlpha
+			Pass
+			{
+				CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+				// make fog work
+				#pragma multi_compile_fog
+
+				#include "UnityCG.cginc"
+
+				struct appdata
+				{
+					float4 vertex : POSITION;
+					float2 uv : TEXCOORD0;
+				};
+
+				struct v2f
+				{
+
+					half3 worldNormal : TEXCOORD2;
+					float2 uv : TEXCOORD0;
+					float3 Reflection : TEXCOORD3;
+					//  UNITY_FOG_COORDS(1)
+					  float4 vertex : SV_POSITION;
+				  };
+
+				  sampler2D _MainTex;
+				  float4 _MainTex_ST;
+
+				  v2f vert(appdata v, float3 normal : NORMAL)
+				  {
+					  v2f o;
+					  o.vertex = UnityObjectToClipPos(v.vertex);
+					  o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+					  o.worldNormal = UnityObjectToWorldNormal(normal);
+					  //o.VSNormal = COMPUTE_VIEW_NORMAL;
+					  UNITY_TRANSFER_FOG(o,o.vertex);
+					  float3 viewDir = UNITY_MATRIX_IT_MV[2].xyz;
+
+					  o.Reflection = reflect(-normalize(viewDir), normalize(o.worldNormal));
+
+					  return o;
+				  }
+
+
+				  samplerCUBE _Cube;
+				  float _Inten;
+				  float3 _Color;
+				  float _Inten2;
+				  float3 _Color2;
+
+				  fixed4 frag(v2f i) : SV_Target
+				  {
+
+					  //to co na laborkach było(cieniowanie)
+					  float4 dirVec = _WorldSpaceLightPos0;
+					  float3 norVec = i.worldNormal;
+
+					  float4 t = dot(normalize(dirVec), normalize(norVec));
+					  t = max(t,0);
+
+					  float3 swiatlo;
+					  swiatlo = _Inten * _Color;
+					  swiatlo += _Inten2 * t *_Color2;
+
+					  swiatlo = min(swiatlo,1);
+
+					  fixed4 col = tex2D(_MainTex, i.uv);//nadanie koloru z tekstury(domyslnie bialy)
+					  col.rgb *= texCUBE(_Cube,normalize(i.Reflection)); //dodanie odbicia 
+
+					  col.rgb *= swiatlo;
+					  col.a = 0.5;
+					  // UNITY_APPLY_FOG(i.fogCoord, col);
+					   return col;
+				   }
+				   ENDCG
+			   }
+		}
+}
