9600ec461d058fb05a0a419e46afd1ed24ffc856
yamaz0 <szulc.patryk1c@gmail.com>
2019-07-09 20:57:23 +0200

    Refrakcja Refleksja

--- SymulatorAEI/Assets/ShaderOnOff.cs
+++ SymulatorAEI/Assets/ShaderOnOff.cs
@@ -5,22 +5,18 @@ using UnityEngine;
 public class ShaderOnOff : MonoBehaviour
 {
     public bool isEnable;
-    public string tag;
-    GameObject[] objs;
+    public GameObject[] objs;
 
     private void Start()
     {
-        objs = GameObject.FindGameObjectsWithTag(tag);
+        
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            
-            foreach (GameObject obj in objs)
-            {
-                obj.SetActive(isEnable);
-            }
+            foreach(GameObject obj in objs)
+            obj.SetActive(isEnable);
         }
     }
 }
